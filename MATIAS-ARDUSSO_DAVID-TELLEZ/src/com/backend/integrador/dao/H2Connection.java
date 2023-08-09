59137c7f592dc441e0a4c78ae6b09209d8e2f306
DavidTG-developer <dtellez43@gmail.com>
2023-08-09 17:27:44 -0500

    9 commit

--- MATIAS-ARDUSSO_DAVID-TELLEZ/src/com/backend/integrador/dao/H2Connection.java
+++ MATIAS-ARDUSSO_DAVID-TELLEZ/src/com/backend/integrador/dao/H2Connection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class H2Connection {
     public static Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("org.H2.Driver");
+        Class.forName("org.h2.Driver");
         return DriverManager.getConnection("jdbc:h2:~/integrador", "sa", "sa");
     }
 }
--- MATIAS-ARDUSSO_DAVID-TELLEZ/src/com/backend/integrador/dao/impl/OdontologoDaoH2.java
+++ MATIAS-ARDUSSO_DAVID-TELLEZ/src/com/backend/integrador/dao/impl/OdontologoDaoH2.java
@@ -1,9 +1,11 @@
 package com.backend.integrador.dao.impl;
 
+import com.backend.integrador.dao.H2Connection;
 import com.backend.integrador.dao.IDao;
 import com.backend.integrador.entity.Odontologo;
 import org.apache.log4j.Logger;
 
+import java.sql.*;
 import java.util.List;
 
 public class OdontologoDaoH2 implements IDao <Odontologo> {
@@ -12,8 +14,53 @@ public class OdontologoDaoH2 implements IDao <Odontologo> {
 
     @Override
     public Odontologo registrar(Odontologo odontologo) {
-        return null;
-    }
+        Connection connection = null;
+        String insert = "INSERT INTO ODONTOLOGOS (MATRICULA, NOMBRE, APELLIDO) VALUES (?, ?, ?)";
+        Odontologo odontologo1 = null;
+        try{
+            connection = H2Connection.getConnection();
+            connection.setAutoCommit(false);
+
+            PreparedStatement ps = connection.prepareStatement(insert, Statement.RETURN_GENERATED_KEYS);
+
+            ps.setInt(1, odontologo.getMatricula());
+            ps.setString(2, odontologo.getNombre());
+            ps.setString(3, odontologo.getApellido());
+            ps.execute();
+
+            connection.commit();
+
+            //select a la base de datos para obtener lo que se registro mas su id
+            odontologo1 = new Odontologo(odontologo.getMatricula(), odontologo.getNombre(), odontologo.getApellido());
+            ResultSet key = ps.getGeneratedKeys();
+            while (key.next()){
+                odontologo1.setId(key.getInt(1));
+            }
+            LOGGER.info("Odontologo Registrado: " + odontologo1);
+
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+            e.printStackTrace();
+            if (connection != null) {
+                try {
+                    connection.rollback();
+                    System.out.println("Tuvimos un problema");
+                    e.printStackTrace();
+                } catch (SQLException exception) {
+                    LOGGER.error(exception.getMessage());
+                    exception.printStackTrace();
+                }
+            }
+        } finally {
+            try {
+                connection.close();
+            } catch (Exception ex) {
+                LOGGER.error("No se pudo cerrar la conexion: " + ex.getMessage());
+            }
+        }
+
+        return odontologo1;
+        }
 
     @Override
     public List<Odontologo> listarTodos() {
